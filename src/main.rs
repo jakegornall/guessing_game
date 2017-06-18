bc0fab7a1889c4cd97e67dc3f80c589f398e797a
Jake Gornall <jakegornall@yahoo.com>
2017-06-18 15:44:10 -0400

    feat: Add ability to play multiple rounds of game
    also, I disable RustAutoComplete in Sublime in order to stop creating all the tmp files in src/.
    I added a score that increments with each round as well.

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,85 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+
+    let mut score = 0;
+    
+    loop {
+
+        score = play_game(score);
+
+        println!("Your Score: {}", score);
+
+        if to_continue() {
+            continue;
+        } else {
+            break;
+        }
+    }
+    
+}
+
+fn to_continue() -> bool {
+    let mut play_again = String::new();
+    let mut result = false;
+    loop {
+        println!("Would you like to play again? (y/n)");
+
+        io::stdin().read_line(&mut play_again)
+            .expect("Error reading input.");
+
+        let play_again = play_again.trim();
+
+        match play_again.as_ref() {
+            "y" => {
+                result = true;
+                println!("Let's play another round!");
+                break;
+            },
+            "n" => {
+                result = false;
+                println!("Good bye!");
+                break;
+            },
+            _ => {
+                println!("Invalid Input!");
+                continue;
+            }
+        }   
+    }
+    result
+}
+
+fn play_game(score: i32) -> i32 {
     println!("Guess the number!");
 
     let secret_num = rand::thread_rng().gen_range(1, 101);
-
-    println!("Secret Number Is: {}", secret_num);
+    let mut new_score = 0;
 
     loop {
-	    println!("Please input your guess:");
+        println!("Please input your guess:");
 
-	    let mut guess = String::new();
+        let mut guess = String::new();
 
-	    io::stdin().read_line(&mut guess)
-	    	.expect("Error reading input.");
+        io::stdin().read_line(&mut guess)
+            .expect("Error reading input.");
 
-	    let guess: u32 = guess.trim().parse()
-	        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
-	    println!("You guessed, {}", guess);
+        println!("You guessed, {}", guess);
 
-	    match guess.cmp(&secret_num) {
-	    	Ordering::Less => println!("Too Small!"),
-	    	Ordering::Greater => println!("Too Big!"),
-	    	Ordering::Equal => {
-	    		println!("You Win!");
-	    		break;
-	    	}
-	    }	
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                new_score = score + 1;
+                break;
+            }
+        }
     }
+    new_score
 }
